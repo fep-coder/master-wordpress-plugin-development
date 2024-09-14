702eeee3019fcc6356060a039dc1c933674adc22
voja <voja@gmail.com>
2024-09-14 11:18:00 +0200

    13 Handle filter submission

---
+++ vk-filter/includes/handle-filter.php
@@ -0,0 +1,79 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class VKF_Handle_Filter
+{
+    public function __construct()
+    {
+        add_action('wp_ajax_filter_posts', [$this, 'filter_posts']);
+        add_action('wp_ajax_nopriv_filter_posts', [$this, 'filter_posts']);
+    }
+
+    public function filter_posts()
+    {
+        $categories = isset($_GET['categories']) ? $_GET['categories'] : [];
+        $meal = isset($_GET['meal']) ? $_GET['meal'] : '';
+        $difficulty = isset($_GET['difficulty']) ? $_GET['difficulty'] : '';
+
+        $args = [
+            'post_type' => 'recipe',
+            'posts_per_page' => -1,
+            'post_status' => 'publish',
+            'tax_query' => [],
+            'meta_query' => [],
+        ];
+
+        // Taxonomy query for categories
+        if (!empty($categories)) {
+            $args['tax_query'][] = [
+                'taxonomy' => 'category',
+                'field' => 'name',
+                'terms' => $categories,
+                'operator' => 'IN',
+            ];
+        }
+
+        // Meta query for meal
+        if (!empty($meal) && $meal !== 'Any') {
+            $args['meta_query'][] = [
+                'key' => 'meal',
+                'value' => $meal,
+                'compare' => '=',
+            ];
+        }
+
+        // Meta query for difficulty
+        if (!empty($difficulty) && $difficulty !== 'Any') {
+            $args['meta_query'][] = [
+                'key' => 'difficulty',
+                'value' => $difficulty,
+                'compare' => '=',
+            ];
+        }
+
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) {
+            echo '<div class="row">';
+            while ($query->have_posts()) {
+                $query->the_post();
+
+                // Output post content
+                echo '<div class="col-6">';
+                echo '<h2><a href="' . get_permalink() . '">' . get_the_title() . '</h2>';
+                if (has_post_thumbnail()) {
+                    echo '<a href="' . get_permalink() . '">'
+                    . get_the_post_thumbnail(get_the_ID(),
+                        'medium', ['class' => 'img-fluid', 'alt' => get_the_title()])
+                        . '</a>';
+                }
+                echo '<div>';
+            }
+            echo '</div>';
+        }
+
+    }
+}
