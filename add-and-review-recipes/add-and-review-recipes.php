fd1d308fea2ec9cdabeb2e89e4de559a92560568
voja <voja@gmail.com>
2024-09-17 10:13:44 +0200

    18 Add and review recipes base class

---
+++ add-and-review-recipes/add-and-review-recipes.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Plugin Name: Add And Review Recipes
+ * Plugin URI: https://vk.com/add-and-review-recipes
+ * Description: Add and review recipes
+ * Version: 1.0
+ * Author: VK
+ * Text Domain: aarr
+ */
+
+// Prevent direct access to file
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('AARR_PATH', plugin_dir_path(__FILE__));
+
+// require_once AARR_PATH . 'includes/list-recipes.php';
+
+class Add_And_Review_Recipes
+{
+    private static $instance = null;
+
+    public function __construct()
+    {
+    }
+
+    public static function get_instance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public static function activate()
+    {
+        // Code to run on plugin activation
+    }
+
+    public static function deactivate()
+    {
+        // Code to run on plugin deactivation
+    }
+
+    public static function uninstall()
+    {
+    }
+}
+
+add_action('plugins_loaded', ['Add_And_Review_Recipes', 'get_instance']);
+
+register_activation_hook(__FILE__, ['Add_And_Review_Recipes', 'activate']);
+register_deactivation_hook(__FILE__, ['Add_And_Review_Recipes', 'deactivate']);
+register_uninstall_hook(__FILE__, ['Add_And_Review_Recipes', 'uninstall']);
